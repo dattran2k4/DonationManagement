ea241212cd230392519475572c6a47988de5a660
dattran2k4 <tranquocthanhdat@gmail.com>
2026-03-15 14:24:49 +0700

    get data systemconfig to home page + about us

--- src/main/java/com/chiaseyeuthuong/controller/AboutController.java
+++ src/main/java/com/chiaseyeuthuong/controller/AboutController.java
@@ -1,18 +1,13 @@
 package com.chiaseyeuthuong.controller;
 
-import com.chiaseyeuthuong.service.SystemConfigService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/about")
 public class AboutController {
-
-    private final SystemConfigService systemConfigService;
 
     @GetMapping
     public String showAboutPage(Model model) {
--- src/main/java/com/chiaseyeuthuong/controller/GlobalControllerAdvice.java
+++ src/main/java/com/chiaseyeuthuong/controller/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package com.chiaseyeuthuong.controller;
 
 import com.chiaseyeuthuong.common.EEventStatus;
 import com.chiaseyeuthuong.service.CategoryService;
+import com.chiaseyeuthuong.service.SystemConfigService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class GlobalControllerAdvice {
 
     private final CategoryService categoryService;
+    private final SystemConfigService systemConfigService;
 
     @ModelAttribute
     public void addCommonData(Model model) {
         model.addAttribute("hello", "world");
         model.addAttribute("statuses", EEventStatus.values());
         model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("systemConfig", systemConfigService.getAllSystemConfigMap());
     }
 }
